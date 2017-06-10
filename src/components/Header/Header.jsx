3db934bb76df5425b852e1c3afff19ee2510da54
katspaugh <ivan@mycs.com>
2017-06-10 20:53:40 +0200

    typings-for-css-modules

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ export default class Header extends React.PureComponent {
         <header>
           <div className={ styles.logo }>
             <Link to={ '/' }>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 72 28">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 72 28" width="72" height="28">
                 <g fill="#333">
                   <rect x="0"  y="0" width="20" height="28" rx="4" ry="4" />
                   <rect x="24" y="0" width="20" height="28" rx="4" ry="4" />
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
 
       {
         test: /\.css$/,
-        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+        loader: 'style!typings-for-css-modules-loader?modules&namedExport&camelCase'
       }
     ]
   },
