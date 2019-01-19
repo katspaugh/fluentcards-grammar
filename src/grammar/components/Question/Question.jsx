6b623cd66a34470a6873845bf53ed9bf940d1e3e
Lyaysan <panda-go-panda@users.noreply.github.com>
2019-01-19 12:59:31 +0100

    Disable second choice (#3)
    * limit selection and show hint
    
    * disable after selecting
    
    * remove true false
    
    * gh-pages
    
    * move CNAME

--- src/grammar/components/Question/Question.jsx
+++ src/grammar/components/Question/Question.jsx
@@ -100,7 +100,7 @@ export default class Question extends React.PureComponent {
         <li key={ i }>
           <button
             className={ classnames(styles.choiceButton, extraClasses) }
-            disabled={ this.state.correct }
+            disabled={ this.state.selectedChoice != null }
             onClick={ () => this.onAnswer(choice) }>
             { choice || '∅' }
           </button>
